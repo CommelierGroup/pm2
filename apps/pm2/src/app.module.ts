4b06db77f9ff79b3795fcf1d1920e943548297ee
sh940701 <visioner2168@gmail.com>
2024-02-14 17:12:12 +0900

    feat: add grpc service

--- apps/pm2/src/app.module.ts
+++ apps/pm2/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'SERVICE_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'service',
+          protoPath: join(__dirname, './service.proto'),
+          url: process.env.SERVICE_URL,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
