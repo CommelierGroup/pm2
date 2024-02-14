6c87b12cd43b5e8dbf01ed62aff3ff8dbd9a97c7
sh940701 <visioner2168@gmail.com>
2024-02-14 15:28:38 +0900

    feat: add GET /service

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 import express from 'express'
+import fetch from 'node-fetch'
 
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
     res.send('Root')
 })
+app.get('/service', async (req, res) => {
+    try {
+        res.send(await fetch('http://localhost:3000').then(res => res.text()))
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({error: 'Unknown error'});
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`application is listening on port ${port}...`)
 })
