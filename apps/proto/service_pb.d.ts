5fe6fa0a6feede2887b98ea2dae1d0280d80d6d3
sh940701 <visioner2168@gmail.com>
2024-02-15 13:19:19 +0900

    feat: add service.proto, ts file

---
+++ apps/proto/service_pb.d.ts
@@ -0,0 +1,41 @@
+// package: service
+// file: service.proto
+
+import * as jspb from "google-protobuf";
+
+export class Empty extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Empty.AsObject;
+  static toObject(includeInstance: boolean, msg: Empty): Empty.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Empty, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Empty;
+  static deserializeBinaryFromReader(message: Empty, reader: jspb.BinaryReader): Empty;
+}
+
+export namespace Empty {
+  export type AsObject = {
+  }
+}
+
+export class Answer extends jspb.Message {
+  getAnswer(): string;
+  setAnswer(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Answer.AsObject;
+  static toObject(includeInstance: boolean, msg: Answer): Answer.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Answer, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Answer;
+  static deserializeBinaryFromReader(message: Answer, reader: jspb.BinaryReader): Answer;
+}
+
+export namespace Answer {
+  export type AsObject = {
+    answer: string,
+  }
+}
+
